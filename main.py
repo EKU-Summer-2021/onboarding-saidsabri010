2a227cc937faafd67a1286d0f5e135efdac5586b
said <eger3612@gmail.com>
2021-06-30 12:21:31 +0200

    fix: test

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import numpy as np
 from src import Polynomial
-from src.read import Issue1
+from src.read import read_csv
 if __name__ == '__main__':
     coeffs = np.array([1,0,0])
     polynom = Polynomial(coeffs)
     print(polynom.evaluate(3))
     print(polynom.roots())
-i = Issue1()
-print(i.read_csv())
+
+
+
+print(read_csv('https://raw.githubusercontent.com/EKU-Summer-2021/intelligent_system_data/main/Intelligent%20System%20Data/CSP/CSP_360.csv'))
--- src/read.py
+++ src/read.py
@@ -3,9 +3,8 @@ import pandas as pd
 """
 reading csv data
 """
-class Issue1:
-    def read_csv(self):
-        print('CSP data : ')
-        df = pd.read_csv(
-            'https://raw.githubusercontent.com/EKU-Summer-2021/intelligent_system_data/main/Intelligent%20System%20Data/CSP/CSP_360.csv')
-        print(df.describe())
+
+def read_csv(source):
+    df = pd.read_csv(source)
+    print(df.describe())
+
