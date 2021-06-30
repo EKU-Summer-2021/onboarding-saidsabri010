5fb9f7251f408d11fa0595de25e779b8e6535e1f
said <eger3612@gmail.com>
2021-06-30 08:54:01 +0200

    fix: adding docstring,rename variables

--- src/read.py
+++ src/read.py
@@ -1,40 +1,42 @@
 import pandas as pd
 
-
 def read_csv():
+    """
+    reading csv data
+    """
     print('CSP data : ')
-    df = pd.read_csv(
+    x = pd.read_csv(
         'https://raw.githubusercontent.com/EKU-Summer-2021/intelligent_system_data/main/Intelligent%20System%20Data/CSP/CSP_360.csv')
-    df2 = pd.read_csv(
+    y = pd.read_csv(
         'https://raw.githubusercontent.com/EKU-Summer-2021/intelligent_system_data/main/Intelligent%20System%20Data/CSP/CSP_53000.csv')
-    df3 = pd.read_csv(
+    m = pd.read_csv(
         'https://raw.githubusercontent.com/EKU-Summer-2021/intelligent_system_data/main/Intelligent%20System%20Data/CSP/CSP_6200.csv')
-    print(df.describe())
-    print(df2.describe())
-    print(df3.describe())
+    print(x.describe())
+    print(y.describe())
+    print(m.describe())
 
     print('-------------------------------')
 
     print('KP data : ')
-    df4 = pd.read_csv(
+    i = pd.read_csv(
         'https://raw.githubusercontent.com/EKU-Summer-2021/intelligent_system_data/main/Intelligent%20System%20Data/KP/KP_10.csv')
-    df5 = pd.read_csv(
+    j = pd.read_csv(
         'https://raw.githubusercontent.com/EKU-Summer-2021/intelligent_system_data/main/Intelligent%20System%20Data/KP/KP_100.csv')
-    df6 = pd.read_csv(
+    k = pd.read_csv(
         'https://raw.githubusercontent.com/EKU-Summer-2021/intelligent_system_data/main/Intelligent%20System%20Data/KP/KP_1000.csv')
-    print(df4.describe())
-    print(df5.describe())
-    print(df6.describe())
+    print(i.describe())
+    print(j.describe())
+    print(k.describe())
 
     print('-------------------------------')
 
     print('TSP data : ')
-    df4 = pd.read_csv(
+    l = pd.read_csv(
         'https://raw.githubusercontent.com/EKU-Summer-2021/intelligent_system_data/main/Intelligent%20System%20Data/TSP/10.csv')
-    df5 = pd.read_csv(
+    n = pd.read_csv(
         'https://raw.githubusercontent.com/EKU-Summer-2021/intelligent_system_data/main/Intelligent%20System%20Data/TSP/100.csv')
-    df6 = pd.read_csv(
+    d = pd.read_csv(
         'https://raw.githubusercontent.com/EKU-Summer-2021/intelligent_system_data/main/Intelligent%20System%20Data/TSP/1000.csv')
-    print(df4.describe())
-    print(df5.describe())
-    print(df6.describe())
+    print(l.describe())
+    print(n.describe())
+    print(d.describe())
--- tests/read.py
+++ tests/read.py
@@ -3,8 +3,10 @@ import unittest
 
 class MyTestCase(unittest.TestCase):
     def test_upper(self):
+        """
+          test
+          """
         self.assertEqual('foo'.upper(), 'FOO')
 
 
-if __name__ == '__main__':
-    unittest.main()
+
