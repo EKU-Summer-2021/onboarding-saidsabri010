bd5a4e37d4baedd5bb9cab764d111d9480f93f58
said <eger3612@gmail.com>
2021-07-01 23:37:58 +0200

    feat:cost function was created + unit test

--- src/cost.py
+++ src/cost.py
@@ -8,9 +8,9 @@ def cost(number_of_items):
      cost method
      """
     list_of_items = []
-    for x in range(0, number_of_items):
+    for item in range(0, number_of_items):
         element = int(input("enter the cost of items"))
         list_of_items.append(element)
-        x += 1
+        item += 1
     total_cost = sum(list_of_items)
     return total_cost
